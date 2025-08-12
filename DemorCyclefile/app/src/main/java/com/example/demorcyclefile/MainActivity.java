a573a727134be998d6b60bdf70cccfcf2de6dfd2
Quang <quang1422003@gmail.com>
2025-08-12 08:59:11 +0700

    Bài 2

---
+++ DemorCyclefile/app/src/main/java/com/example/demorcyclefile/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.demorcyclefile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    public MainActivity() {
+        super();
+    }
+
+    Button btncall;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this," CR424 - onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"CR424 - on Destroy()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this," CR424 - onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this," CR424 - onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this," CR424 - onStart", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this," CR424 - onStop", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                Intent intent1 = new Intent(MainActivity.this, subactivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
