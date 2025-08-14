f42f1b1a064a258fd62fdd315369d88c8a9133dc
Quang <quang1422003@gmail.com>
2025-08-15 00:32:19 +0700

    Bài 11

---
+++ IntentFiler/app/src/main/java/com/example/intentfiler/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.example.intentfiler;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+        try {
+            WebView webView = findViewById(R.id.webview1);
+            webView.loadUrl(data.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
