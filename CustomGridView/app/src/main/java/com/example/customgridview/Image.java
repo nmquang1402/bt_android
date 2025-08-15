55ba7e33fd4174e04baf383c47f5505bf81e2dd9
Quang <quang1422003@gmail.com>
2025-08-15 22:34:01 +0700

    Bài 13.4

---
+++ CustomGridView/app/src/main/java/com/example/customgridview/Image.java
@@ -0,0 +1,25 @@
+package com.example.customgridview;
+
+public class Image {
+    private int img;
+    private String name;
+    public int getImg() {
+        return img;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setImg(int img) {
+        this.img = img;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Image(int img, String name) {
+        this.img = img;
+        this.name = name;
+    }
+    public Image() {
+    }
+}
+
